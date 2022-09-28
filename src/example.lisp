f0c81954ada06c69184e84a511887a0bafb983c2
hxz <herongxian@163.com>
2022-09-28 23:22:59 +0800

    move utils to example

--- src/example.lisp
+++ src/example.lisp
@@ -1,5 +1,5 @@
 ;;;; A full demo of websocket handlers.
-;;;; Most of the code were copied from clog <https://github.com/rabbibotton/clog>
+;;;; Most of the code were copied from Clog <https://github.com/rabbibotton/clog>
 ;;;; This demo defines four handlers:
 ;;;;   HANDLE-NEW-CONNECTION for open event,
 ;;;;   HANDLE-MESSAGE for message event, and ECHO-MESSAGE is a simpler handler for message event,
@@ -11,15 +11,98 @@
 ;;;;
 ;;;; An html src is also put in the #|...|# comment in the end of this file,
 ;;;; it will display all messages sending from the websocket server.
-;;;; This html src is from The Common Lisp Cookbook in <https://lispcookbook.github.io/cl-cookbook/websockets.html>.
+;;;; This html src is from "The Common Lisp Cookbook" in <https://lispcookbook.github.io/cl-cookbook/websockets.html>.
 ;;;; Save the src into an html file, change the ws address to your websocket address,
 ;;;; then open it with a web browser and see what will be displayed.
 ;;;;
 ;;;; Hope it will be a good starting point to develop a websocket application.
 
 
+(ql:quickload (list :atomics
+                    :cl-isaac
+                    :bordeaux-threads
+                    :cl-ppcre
+                    :easy-websocket))
+
+
 (in-package :easy-websocket)
 
+;;; utils
+
+;; Random
+
+#-(or mswindows win32 cormanlisp) ; Not supported yet
+(defparameter *isaac-ctx*
+  (isaac:init-self-seed :count 5
+                        :is64 #+:X86-64 t #-:X86-64 nil)
+  "Used to generate random hex strings")
+
+(defun random-hex-string ()
+  "Generate cryptographic grade random ids for use in connections."
+  #+(or mswindows win32 cormanlisp)
+  (ironclad:byte-array-to-hex-string
+   (ironclad:random-data 16))
+  #-(or mswindows win32 cormanlisp)
+  (format nil "~(~32,'0x~)" (#+:X86-64 isaac:rand-bits-64
+                             #-:X86-64 isaac:rand-bits
+                             *isaac-ctx* 128)))
+
+;; Hash table
+
+(defun make-sync-hash-table (&rest args)
+  "Make synchronized hash table"
+  #+(or sbcl ecl mezzano)
+  (apply #'make-hash-table :synchronized t args)
+  #-(or sbcl ecl mezzano) (apply #'make-hash-table args))
+
+;; Atomic
+
+(defun make-atomic (init-value)
+  "Return a structure that can be cas'ed"
+  #+ccl
+  (make-array 1 :initial-element init-value)
+  #-ccl
+  (cons init-value nil))
+
+(defmacro atomic-place (atomic-structure)
+  "Return the place of the value of ATOMIC-STRUCTURE which is made by make-atomic."
+  #+ccl
+  `(svref ,atomic-structure 0)
+  #-ccl
+  `(car ,atomic-structure))
+
+(defmacro atomic-incf (place &optional (diff 1))
+  "Atomic incf the fixnum in PLACE with DIFF and return the old value."
+  #+sbcl
+  `(sb-ext:atomic-incf ,place ,diff)
+  #+ccl
+  `(let ((old ,place))
+     (ccl::atomic-incf-decf ,place ,diff)
+     old))
+
+(defun make-counter (&optional (init-num 0))
+  "Make an thread safe counter which starts from INIT-NUM."
+  (declare (fixnum init-num))
+  (make-atomic init-num))
+
+(defmacro counter-incf (counter)
+  "Increase COUNTER and return its old value"
+  `(atomic-incf (atomic-place ,counter)))
+
+(defmacro counter-peek (counter)
+  "Atomically get the value of PLACE without change it."
+  #+sbcl
+  `(progn
+     (sb-thread:barrier (:read))
+     (atomic-place ,counter))
+  #-sbcl
+  (alexandria:with-gensyms (val)
+    `(loop for ,val = (atomic-place ,counter)
+           until (atomics:cas (atomic-place ,counter) ,val ,val)
+           finally (return ,val))))
+
+
+;;; -------
 
 (defvar *connection->id* (make-sync-hash-table)
   "A map from websocket objects to their id strings")
@@ -81,6 +164,7 @@
   (remove-connection-data conn-id)
   (log:info "Connection <~d> has been removed." conn-id))
 
+;; handlers
 
 (defun handle-new-connection (conn-obj env)
   "Handle new incoming websocket CONNECTIONS with ID from boot page."
@@ -125,7 +209,7 @@
         (log:error "Condition caught in handle-new-connection - ~A." c)
         (values 0 c)))))
 
-;; A very simple message handler
+;; A very simple handler for :messagen event
 (defun echo-message (conn-obj message)
   "Send the message back to the client"
   (websocket-driver:send conn-obj message))
@@ -216,7 +300,7 @@
         (clrhash *id->connection*)))
 
 
-;;; Helpers
+;; Helpers
 
 (defun inspect-connection (conn-id)
   (let ((conn-data (gethash conn-id *id->connection-data*)))
