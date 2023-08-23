688c055da1046e0ae8c1c80deb1ce3fbe149cf0a
hxz <herongxian@163.com>
2023-08-23 17:49:27 +0800

    setup-server renamed to handle-websocket-connection, start add verify and http-handler parameters

--- src/websocket.lisp
+++ src/websocket.lisp
@@ -11,8 +11,8 @@
 
 
 ;;; Server
-
-(defun setup-server (on-open-handler on-message-handler on-error-handler on-close-handler env)
+;;; clog-server
+(defun handle-websocket-connection (on-open-handler on-message-handler on-error-handler on-close-handler env)
   "Setup websocket server on ENV with OPEN, MESSAGE, ERROR and CLOSE handlers."
   (handler-case
       (let ((ws (websocket-driver:make-server env)))
@@ -22,14 +22,14 @@
                                  (handler-case
                                      (funcall on-open-handler ws env)
                                    (condition (c)
-                                     (log:error "Setup-server condition on :open with <~A>" c)
+                                     (log:error "Websocket connection handler on :open with condition <~A>" c)
                                      (values 0 c))))))
         (websocket-driver:on :message ws
                              (lambda (msg)
                                (handler-case
                                    (funcall on-message-handler ws msg)
                                  (condition (c)
-                                   (log:error "Setup-server condition on :message with <~A>" c)
+                                   (log:error "Websocket connection handler on :message with condition <~A>" c)
                                    (values 0 c)))))
         (when on-error-handler
           (websocket-driver:on :error ws
@@ -37,7 +37,7 @@
                                  (handler-case
                                      (funcall on-error-handler ws err)
                                    (condition (c)
-                                     (log:error "Setup-server condition on :error with <~A>" c)
+                                     (log:error "Websocket connection handler on :error with condition <~A>" c)
                                      (values 0 c))))))
         (when on-close-handler
           (websocket-driver:on :close ws
@@ -46,15 +46,16 @@
                                  (handler-case
                                      (funcall on-close-handler ws)
                                    (condition (c)
-                                     (log:error "Setup-server condition on :close with <~A>" c)
+                                     (log:error "Websocket connection handler on :close with condition <~A>" c)
                                      (values 0 c))))))
         (lambda (responder)
           (declare (ignore responder))
           (websocket-driver:start-connection ws)))
     (condition (c)
-      (log:error "Setup-server condition when initializing the server with <~A>" c)
+      (log:error "Websocket connection handler failed when initializing the server with condition <~A>" c)
       (values 0 c))))
 
+;; initialize
 (defun start (on-open-handler
               on-message-handler
               on-error-handler
@@ -65,7 +66,7 @@
                 (port    8080)
                 (server  :hunchentoot)
                 (workers 2)
-                &allow-other-keys)
+              &allow-other-keys)
   "Builds a clack app and then starts the webserver.
 Only websocket requests are allowed, which have upgrade/websocket record in the headers table,
 all other requests will be responded with code 403.
@@ -79,6 +80,10 @@ port: the port opened by this server
 server: can be one of :hunchentoot, :woo, :wookie, default to :hunchentoot, bugs occurred for others.
 workers: the count of threads which will be used to fork the workers of the webserver.
 
+furthermore, there are two additional keyword argument can be passed.
+verifier: a unary function and pass env to check the request. This function will be called if it's provided.
+http-handler: a unary function and pass env to serve the normal http requests. This function will be called if it's provided.
+
 on-open-handler, on-error-handler and on-close-handler can be nil to ignore the respect events.
 
 Note that the default webserver is hunchentoot, others still have bugs or strange behaviors.
@@ -86,17 +91,30 @@ For example, for WOO server, sending messages to the client in on-open-handler w
 however, this will not cache for HUNCHENTOOT.
 "
   (setf *app* (lack:builder
+               ;; filter request
                (lambda (app)
                  (lambda (env)
-                   (log:info "A remote host \"~d:~d\" tries to initialize a connection to this server with request \"~d\"."
-                             (getf env :REMOTE-ADDR) (getf env :REMOTE-PORT) (getf env :REQUEST-URI))
+                   (log:info "A remote host \"~d:~d\" tries to make a \"~d\" request for uri \"~d\"."
+                             (getf env :REMOTE-ADDR) (getf env :REMOTE-PORT) (getf env :URL-SCHEME) (getf env :REQUEST-URI))
                    (let ((headers (getf env :headers)))
                      (if (equal "websocket" (gethash "upgrade" headers))
-                         (funcall app env)
-                         (prog1 '(403 (:content-type "text/plain") ("")) ; forbide other requests than websocket
-                           (log:warn "The client tried to make a normal http connection to this websocket server."))))))
+                         ;; check if there's a request verify function in the args
+                         (alexandria:if-let (verify-request (getf args :verifier))
+                           ;; the url has the fmt such as "ws://localhost/ws?r=conn_id" where conn_id should be verified
+                           (if (funcall verify-request env)
+                               (funcall app env)
+                               (prog1 '(403 (:content-type "text/plain") ("403 Forbidden!"))
+                                 (log:warn "Failed to verify the websocket connection request.")))
+                           (funcall app env))
+                         ;; this server doesn't deserve to serve http requests,
+                         ;; however, the http-handler parameter gives a chance to serve normal http requests.
+                         (alexandria:if-let (http-handler (getf args :http-handler))
+                           (funcall http-handler env)
+                           (prog1 '(403 (:content-type "text/plain") ("403 Forbidden!")) ; forbide other requests than websocket
+                             (log:warn "The client tried to make a normal http connection to this websocket server.")))))))
+               ;; handle websocket
                (lambda (env)
-                 (setup-server on-open-handler on-message-handler on-error-handler on-close-handler env))))
+                 (handle-websocket-connection on-open-handler on-message-handler on-error-handler on-close-handler env))))
   (setf *handler* (apply #'clack:clackup
                          *app*
                          :server server
@@ -106,8 +124,7 @@ however, this will not cache for HUNCHENTOOT.
                          :use-default-middlewares nil
                          :worker-num workers
                          (alexandria:delete-from-plist args :server :address :port :debug :use-default-middlewares :worker-num)))
-  (log:info "Websocket server started at address \"~A:~A\" within webserver \"~d\" with ~d workers."
-            host port server workers)
+  (log:info "Websocket server started at address \"~A:~A\" with ~d workers." host port workers)
   *handler*)
 
 (defmacro stop (&body cleanup)
